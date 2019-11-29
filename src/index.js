b3d99da7156a904ec35afc8417c1c7c3fed75e6d
linyi <linyi@tsign.cn>
2019-11-29 15:42:52 +0800

    feat: 修改模块输出规范

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import * as base from "./base"
 import * as date from "./date"
 
-export function toNumber(val) {
+export default function toNumber(val) {
     const n = parseFloat(val)
     return isNaN(n) ? val : n
-}
-
-export default {
-    base,
-    date
 }
