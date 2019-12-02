e3d2bdd53d8271604981ca0c1d2a076cce741109
linyi <linyi@tsign.cn>
2019-12-02 14:49:18 +0800

    feat: 调整模块输出

--- src/base/index.js
+++ src/base/index.js
@@ -13,3 +13,8 @@ export function isObject(obj) {
 export function isPlainObject(obj) {
     return Object.prototype.toString.call(obj) === "[object, Object]"
 }
+
+export function toNumber(val) {
+    const n = parseFloat(val)
+    return isNaN(n) ? val : n
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import * as base from "./base"
 import * as date from "./date"
 
-function toNumber(val) {
-    const n = parseFloat(val)
-    return isNaN(n) ? val : n
-}
-
 export default {
     base,
-    date,
-    toNumber
+    date
 }
